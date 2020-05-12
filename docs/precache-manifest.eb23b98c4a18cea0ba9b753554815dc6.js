461f391d506dfb4866db6351ad5e39c4ac3991f1
Fernando Herrera <fernando.herrera85@gmail.com>
2020-05-12 18:14:14 -0400

    docs agregado

---
+++ docs/precache-manifest.eb23b98c4a18cea0ba9b753554815dc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf2c87e70957886ab942639a586aab89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e20937264c9833a551ed",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e20937264c9833a551ed",
+    "url": "/static/js/main.6f6a5c6d.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
